cfc29e8ab97d632095991cd0f463d2c4ce6cc487
nazar <nazar.khort@gmail.com>
2021-08-22 14:33:40 +0300

    The response of say_hello_handler function has been changed to make sure lambda is working correctly.

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import json
 def say_hello_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Nazar!')
+        'body': json.dumps('Hello World')
     }
