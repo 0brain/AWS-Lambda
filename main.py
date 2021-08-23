e42654ed8842d48c3f2e4dbcc24d9918e6e870bb
nazar <nazar.khort@gmail.com>
2021-08-23 17:36:50 +0300

    The function should display a list of titles.

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
+import requests
 
 
 def say_hello_handler(event, context):
+    r = requests.get('https://api.airtable.com/v0/apprs1kK3yiSRCQeX/MainTable?api_key=keyu4O4iQfg69w0iQ')
+    data = r.json()["records"]
+    l = [(el["fields"]["title"], el["fields"]["ID"]) for el in data]
+    res = [el[0] for el in sorted(l, key=lambda x: x[1])]
     return {
         'statusCode': 200,
         'body': json.dumps('Hello World')
-    }
+    }, "<br>", res
