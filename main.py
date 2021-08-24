bf1388ba65a96ec1101d18b499a129ad205241e0
nazar <nazar.khort@gmail.com>
2021-08-24 13:11:20 +0300

    output of three elements

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import json
 import requests
 
 
-def say_hello_handler(event, context):
+def call_table():
     r = requests.get('https://api.airtable.com/v0/apprs1kK3yiSRCQeX/MainTable?api_key=keyu4O4iQfg69w0iQ')
     data = r.json()["records"]
     l = [(el["fields"]["title"], el["fields"]["ID"]) for el in data]
-    res = [el[0] for el in sorted(l, key=lambda x: x[1])]
+    return [el[0] for el in sorted(l, key=lambda x: x[1])]
+
+
+def say_hello_handler(event, context):
+    if 'globalCursor' not in globals():
+        global globalCursor
+        globalCursor = 0
+    table = call_table()
+    result = table[globalCursor] + "<br>" + table[globalCursor + 1] + "<br>" + table[globalCursor + 2]
+    globalCursor += 1
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello World')
-    }, "<br>", res
+        "statusCode": 200,
+        "body": result,
+        "headers": {
+        'Content-Type': 'text/html'
+        }}
+
